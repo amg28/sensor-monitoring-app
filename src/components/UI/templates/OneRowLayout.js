e2820ea559bcf66da29bf77d10176dcbca8addaa
Arturs Bonfelds <arturs.bonfelds@testdevlab.com>
2021-05-13 21:05:23 +0300

    floor plans

---
+++ src/components/UI/templates/OneRowLayout.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Paper from '@material-ui/core/Paper';
+import Grid from '@material-ui/core/Grid';
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+        flexGrow: 1,
+        marginTop: '80px',
+        marginLeft: '90px',
+        marginRight: '20px'
+
+    },
+    paper: {
+        padding: theme.spacing(2),
+        textAlign: 'center',
+        color: theme.palette.text.secondary,
+        minHeight: '300px'
+    }
+}));
+
+export default function OneRowLayout({floorPlan}) {
+    const classes = useStyles();
+
+    return (
+        <div className={classes.root}>
+          <Grid container spacing={3}>
+            <Grid item xs={7}>
+              {floorPlan.map((component) => <Paper className={classes.paper}>{component}</Paper>)}
+            </Grid>
+            <Grid item xs={5}>
+              <Paper className={classes.paper}><h1>Info</h1></Paper>
+            </Grid>
+          </Grid>
+        </div>
+    );
+}
--- src/components/pages/BuildingPlan.js
+++ src/components/pages/BuildingPlan.js
@@ -1,10 +1,19 @@
 import React from 'react'
+import TwoColumnLayout from "../UI/templates/TwoColumnLayout";
+import OneRowLayout from "../UI/templates/OneRowLayout";
 import Navigation from '../UI/organisms/Navigation'
+import Floor1 from '../UI/atoms/Floor1';
+import Floor2 from '../UI/atoms/Floor2';
 
 function BuildingPlan() {
+
+  const floor1 = [(<Floor1 />)];
+  const floor2 = [(<Floor2 />)];
     return (
         <div>
             <Navigation pageTitle="Building Plan" />
+            <OneRowLayout floorPlan={floor1} />
+            <OneRowLayout floorPlan={floor2} />
         </div>
     )
 }
